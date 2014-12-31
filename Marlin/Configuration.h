833069df8a25894a684c44bc112a765bc103995a
Lane Roberts <nolatari@vandarin.net>
2014-12-31 11:42:24 -0600

    decrease z home feedrate, increase X/Y federates and acceleration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -478,14 +478,14 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 
 //// MOVEMENT SETTINGS
 #define NUM_AXIS 4 // The axis order in all axis related arrays is X, Y, Z, E
-#define HOMING_FEEDRATE {50*60, 50*60, 50*60, 0}  // set the homing speeds (mm/min)
+#define HOMING_FEEDRATE {50*60, 50*60, 30*60, 0}  // set the homing speeds (mm/min)
 
 // default settings
 
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   {78.7402,78.7402,200.0*8/3,760*1.1}  // default steps per unit for Ultimaker
 #define DEFAULT_AXIS_STEPS_PER_UNIT   {44.4444, 44.4444, 88.88888, 95} // Phoenix
-#define DEFAULT_MAX_FEEDRATE          {200, 100, 150, 25}    // (mm/sec)
-#define DEFAULT_MAX_ACCELERATION      {2500,500,1000,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
+#define DEFAULT_MAX_FEEDRATE          {200, 140, 100, 45}    // (mm/sec)
+#define DEFAULT_MAX_ACCELERATION      {2500,1500,2000,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
 #define DEFAULT_ACCELERATION          1000    // X, Y, Z and E max acceleration in mm/s^2 for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  3000   // X, Y, Z and E max acceleration in mm/s^2 for retracts
