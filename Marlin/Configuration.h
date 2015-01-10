f535019bbcddc35f274ac74415aa6c82e8ea66e4
Lane Roberts <nolatari@vandarin.net>
2015-01-10 15:43:31 -0600

    new offset after tightening Z axis

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -404,7 +404,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
   // X and Y offsets must be integers
   #define X_PROBE_OFFSET_FROM_EXTRUDER -15
   #define Y_PROBE_OFFSET_FROM_EXTRUDER -14
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -15.3
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -14.9
 
   #define Z_RAISE_BEFORE_HOMING 25       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
